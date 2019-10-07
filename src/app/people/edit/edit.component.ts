04236c20b2bdf54f26ea38b1cd3e4b26b66caed7
Juan Carlos <jcyovera@gmail.com>
2019-10-07 21:47:19 +0000

    Deployment o QA

--- src/app/people/edit/edit.component.ts
+++ src/app/people/edit/edit.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatFormFieldControl,
+  MatFormField, MatDialog
+} from "@angular/material";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Person } from "../shared/components/person/person";
 
 @Component({
-  selector: 'app-edit',
-  templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  selector: "app-edit",
+  templateUrl: "./edit.component.html",
+  styleUrls: ["./edit.component.scss"]
 })
 export class EditComponent implements OnInit {
+  id: number;
+  constructor(
+    public dialogRef: MatDialogRef<EditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Person
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onNoClick(): void{
+    this.dialogRef.close();
   }
-
 }
--- src/app/people/list/list.component.ts
+++ src/app/people/list/list.component.ts
@@ -1,15 +1,103 @@
-import { Component, OnInit } from '@angular/core';
+import { EditComponent } from "./../edit/edit.component";
+import { PeopleService } from "./../shared/services/people.service";
+import {
+  Component,
+  OnInit,
+  Inject,
+  ViewChild,
+  AfterViewInit
+} from "@angular/core";
+import { Person } from "../shared/components/person/person";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatPaginator,
+  MatTableDataSource,
+  PageEvent,
+  MatIcon
+} from "@angular/material";
+import { InspectComponent } from "../inspect/inspect.component";
+import { OverlayContainer } from "@angular/cdk/overlay";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.scss"]
 })
 export class ListComponent implements OnInit {
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  dataSource: MatTableDataSource<Person>;
+  people: Person[];
+  displayedColumns: string[] = [
+    "Nombre",
+    "Documento",
+    "Tipo Documento",
+    "Buttons"
+  ];
+  person: Person;
+  selected;
 
-  constructor() { }
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
 
-  ngOnInit() {
+  pageEvent: PageEvent;
+
+  selectedRowIndex: number = -1;
+
+  constructor(
+    private peopleService: PeopleService,
+    public dialog: MatDialog,
+    overlayContainer: OverlayContainer
+  ) {
+    overlayContainer.getContainerElement().classList.add("mat-light-theme");
   }
 
+  ngOnInit() {
+    this.peopleService.getPeople().subscribe(people => {
+      this.people = people;
+      console.log(this.people);
+      this.dataSource = new MatTableDataSource(this.people);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    this.pageSizeOptions = setPageSizeOptionsInput.split(",").map(str => +str);
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  openInfo(person: Person): void {
+    const dialogRef = this.dialog.open(EditComponent, {
+      width: "585px",
+      height: "520px",
+      data: person
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("The dialog was closed");
+    });
+  }
+
+  delete(person: Person): void {
+    console.log(person.id);
+    console.log(this.selected);
+  }
+
+  highlight(row) {
+    this.selectedRowIndex = row.id;
+    console.log(this.paginator.pageIndex, this.paginator.pageSize);
+    const dialogRef = this.dialog.open(InspectComponent, {
+      width: "585px",
+      height: "520px",
+      data: row
+    });
+  }
 }
