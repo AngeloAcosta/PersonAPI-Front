91ffebbaba891c85a7596b900e3017fc994f2b9c
Hardi Alexander Manrique Hurtado <hmanrique@belatrixsf.com>
2019-10-24 13:30:52 +0000

    List kinship

--- src/app/kinships/shared/service/kinships.service.ts
+++ src/app/kinships/shared/service/kinships.service.ts
@@ -1,9 +1,10 @@
-import { Kinship } from 'src/app/models/kinship.model';
+import { ApiKinship, KinshipModel } from 'src/app/models/kinship.model';
 import { environment } from './../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
 import { KinshipRelation } from 'src/app/models/kinship.model';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 const httpOptions = {
   headers : new HttpHeaders({
@@ -18,26 +19,40 @@ export class KinshipsService {
   constructor(private http: HttpClient) {}
   kinshipsUrl: string = environment.baseUrl + '/kinships';
 
-  getKinship(): Observable<Kinship[]> {
-    return this.http.get<Kinship[]>(`${this.kinshipsUrl}`).pipe();
+  getKinships(): Observable<KinshipModel[]> {
+    return this.http.get<ApiKinship>(`${this.kinshipsUrl}`).pipe(
+      map((res: ApiKinship) => {
+        return res.data;
+      })
+    );
   }
-  getKinshipsSorted(order, type): Observable<Kinship[]> {
+  getKinshipsSorted(order, type): Observable<KinshipModel[]> {
     const url = this.kinshipsUrl;
     let params = new HttpParams();
     params = params.append('orderBy', order);
     params = params.append('orderType', type);
-    return this.http.get<Kinship[]>(`${url}`, { params }).pipe();
+    return this.http.get<ApiKinship>(`${url}`, { params }).pipe(
+      map((res: ApiKinship) => {
+        return res.data;
+      })
+    );
   }
-  editKinship(kinship: Kinship): Observable<Kinship> {
-    const url = `${this.kinshipsUrl}/${kinship.id}`;
-    return this.http.put<Kinship>(url, kinship, httpOptions);
+  editKinship(kinship: KinshipModel): Observable<KinshipModel> {
+    const url = `${this.kinshipsUrl}/${kinship.idKinship}`;
+    return this.http.put<KinshipModel>(url, kinship, httpOptions);
   }
   deleteKinship(kinship: any) {
-    const url = `${this.kinshipsUrl}/${kinship.id}`;
+    const url = `${this.kinshipsUrl}/${kinship.idKinship}`;
     return this.http.delete(url, httpOptions);
   }
   addKinship(kinship: KinshipRelation) {
   const url = `${this.kinshipsUrl}`;
   return this.http.post<KinshipRelation>(url, kinship, httpOptions);
-}
+  }
+  getKinship(kinship: KinshipModel): Observable<KinshipModel[]> {
+    return this.http.get<ApiKinship>(`${this.kinshipsUrl}/${kinship.idKinship}`).pipe(
+      map((res: ApiKinship) => {
+        return res.data;
+      }));
+  }
 }
